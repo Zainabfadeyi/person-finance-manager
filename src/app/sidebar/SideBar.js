29e6fa91f56254efb750407605a7d6c8dbcdfa51
Zainab Omolola Fadeyi <70825458+Zainabfadeyi@users.noreply.github.com>
2024-07-22 20:35:59 +0100

    push

---
+++ src/app/sidebar/SideBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './SideBar.module.css'
+
+const SideBar = () => {
+  return (
+    <div className={styles.SideBar} >SideBar</div>
+  )
+}
+
+export default SideBar
